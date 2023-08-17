8c89eb612e1d64ada69041b04c262ae19e3af392
Tania Loaiza <taniaberenice_124@hotmail.com>
2023-08-17 14:50:53 -0700

    Basic design for split pdf window

---
+++ PdfTools/Data/SplitPDF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfTools.Data
+{
+    internal class SplitPDF
+    {
+        public string Name { get; set; }
+        public string Order { get; set; }
+        public int Page { get; set; }
+        public int Level { get; set; }
+    }
+}
